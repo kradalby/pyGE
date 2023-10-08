dffc3fd44219cd2d87abb27207c2768686cea958
Kristoffer Dalby <kristoffer@dalby.cc>
2023-10-08 17:13:24 +0200

    use non-classic ge
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- pyGE.py
+++ pyGE.py
@@ -2,28 +2,23 @@ import requests
 
 
 class GE:
-
     def __init__(self, party=None, user=None, password=None):
         if party is None:
-            raise TypeError('party is not set')
+            raise TypeError("party is not set")
         if user is None:
-            raise TypeError('user is not set')
+            raise TypeError("user is not set")
         if password is None:
-            raise TypeError('password is not set')
+            raise TypeError("password is not set")
 
-        self.BASEURL = 'https://classic.geekevents.org'
+        self.BASEURL = "https://geekevents.org"
         self.party = party
         self.user = user
         self.password = password
 
     def get(self, endpoint):
-        url = '{}/{}/{}/'.format(
-            self.BASEURL,
-            self.party,
-            endpoint
-        )
+        url = "{}/{}/{}/".format(self.BASEURL, self.party, endpoint)
         resp = requests.get(url, auth=(self.user, self.password))
         return resp.json()
 
     def get_crew(self):
-        return self.get('crew/api/crew-list')
+        return self.get("crew/api/crew-list")
